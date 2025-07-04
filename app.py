16c8f1d78689d068cf87a019c75e5669c88334c6
Jahnavich-04 <chjanu1518@gmail.com>
2025-07-04 16:07:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request, jsonify
 import nltk
 import random
 import string
+import os
 from nltk.stem import WordNetLemmatizer
+
 
 # Download NLTK resources
 nltk.download('punkt')
@@ -102,4 +104,6 @@ def get_bot_response():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets the PORT env variable
+    app.run(host="0.0.0.0", port=port, debug=True)
+    
